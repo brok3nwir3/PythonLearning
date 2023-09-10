fa4092e709155d128173c85da11fe69c1e846154
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2023-09-10 13:12:31 -0700

    Week 3 assignment

---
+++ wk3_assignment.py
@@ -0,0 +1,74 @@
+# Answer the following questions. Remove the commenting, to run or work on the code.
+
+# Q1
+# What will be returned by the print statement?
+#fruits = ["apple", "banana", "cherry", "kiwi", "grape", "lemon"]
+#print(fruits[-2])
+
+# Q2
+# What will be returned by the print statement?
+#fruitie = ("apple", "banana", "cherry", "kiwi", "grape", "lemon")
+#print(fruitie[0:2])
+# Explanation...
+# https://stackoverflow.com/questions/509211/how-slicing-in-python-works
+
+# Q3
+# What are TWO ways the print statement could be written, to select "cherry" through "lemon"?
+#cool_fruits = ["apple", "banana", "cherry", "kiwi", "grape", "lemon"]
+#print(cool_fruits[?])
+#print(cool_fruits[?])
+
+# Q4
+# What does this print statement return?
+#fave_dog = "poodle"
+#fave_food = "pizza"
+#print("Bob's favorite dog is " + fave_dog + " and his favorite food is " + fave_food)
+
+# Q5
+# Alice is getting an error with the following code. Help her modify her code, to get it working.
+#materials = ("brick", "concrete", "wood")
+#materials[0] = "iron"
+#print(materials())
+
+# Q6
+# Bob is getting an error with the following code. Help him modify his code, to get it working.
+#very_cool_names = {"John", "Sally", "Greg", "Mike"}
+#print(very_cool_names[0])
+
+# Q7
+# What will the following print statement return? Why?
+#txt = "orange banana"
+#x = txt.strip("orange ")
+#print("Of all fruits", x, "is my favorite.")
+
+# Q8
+# Alice wants to return the first three items in her tuple, but she's getting a syntax error. Help Alice fix her code.
+#a = ("a", "b", "c", "d", "e", "f", "g", "h")
+#x = slice(0:3)
+#print(a[x])
+
+# Q9
+# Bob is learning how to use "if" statements, but his code is throwing an error. Help him fix his code.
+#a = 33
+#b = 200
+#if b > a:
+#print("b is greater than a")
+
+# Q10
+# Alice has assigned points to her favorite birds.
+# She then created a program to prove seagulls are the best bird.
+# However, she needs help fixing her final "if" statement.
+# Help Alice finish her code.
+#eagle = 150
+#parrot = 300
+#falcon = 200
+#seagull = 500
+#penguin = 350
+#if seagull > parrot:
+#    print("A seagull is greater than parrot.")
+#if falcon > eagle:
+#    print("A falcon is greater than an eagle.")
+#if seagull > falcon:
+#    print("But seagull is even greater than a falcon.")
+#if seagull > eagle ? seagull > parrot ? seagull > falcon ? seagull > penguin:
+#    print("Clearly, seagulls are the greatest of all birds.")
