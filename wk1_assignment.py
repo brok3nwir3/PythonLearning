abfe2a0709717a8c9c90c157775374f7720255bd
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2023-09-18 14:45:41 -0700

    name update

---
+++ wk1_assignment.py
@@ -0,0 +1,17 @@
+#Hello World Program.
+print("Hello, World!")
+
+#Testing...
+print(7)
+print("a")
+
+#Set a variable.
+my_var = 8
+
+#Print a variable.
+print(my_var)
+
+my_var = my_var * 2
+
+#We want to print the value of my_var * 2.
+print(my_var)
