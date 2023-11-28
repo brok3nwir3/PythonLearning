471c48ac4ff0b5d7a386b09aaade59a9e2b3633f
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2023-11-27 22:24:06 -0800

    Week 8 Assignment

---
+++ wk8_assignment.py
@@ -0,0 +1,83 @@
+# Week 8 - Exploring the OS module and continuing string manipulation.
+
+# Hint: This website will be useful for some of the questions...
+# https://www.freecodecamp.org/news/python-string-manipulation-handbook
+
+# Q1
+# Write a single line of code that replaces all instances of “dog” in the following sentence variable, with “seagull” instead.
+
+sentence = 'dogs are super cool and dogs are the best animal.'
+
+
+# Q2
+# Alices' team is only allowed to have comments in their code that are
+# A maximum of 50 characters long.
+# Help Alice convert her long comment into a comment that spans more lines,
+# doesn't break the 60 character limit, and doesn't use the ‘#' character.
+# Alice's comment...
+
+# My name is Alice. I wrote this program in the year 2023, while I worked at Seagull Inc. My program checks a long list of awesome animals and gives them a score, based on how awesome they are.
+
+
+# Q3
+# Bob needs to reformat a crazy string that he received from a new system.
+# Help Bob write a program that prints a cleaned up version of the variable.
+# The program should convert all upper case letters to lowercase,
+# remove all hyphens, and split the string whenever a period appears.
+# Bonus: Have the program print each of the final words. 
+
+crazyString = 'e-R-Ror.oU-tp-Ut.i-s.O-F-T-en.diF-Fi-cult.T-O.rea-D.Wi-tHouT.ReFo-rMA-ttIn-G'
+
+
+# Q4
+# Alice learned about the OS module, which allows a Python program to execute
+# Linux OS commands. Help Alice use the OS library to create
+# a program that returns output similar to the following when run…
+# “The current user is: X”
+# “The working directory is: Y”
+# Bonus: Have the output on two lines (like the example above^), instead of four.
+
+
+# Q5
+# Bob needs some help writing a program to clean up data.
+# The program should iterate through his list of variables.
+# Whenever the program encounters the string ‘ake' it should
+# be removed from the entry. And whenever the program
+# encounters the letter ‘w' or ‘t' it should be removed.
+
+item1 = 'He finished playing without a single mistake.'
+item2 = 'For the sake of Jake we started clapping.'
+item3 = 'She was waiting on the god forsaken island.'
+bobs_list = [item1, item2, item3]
+
+
+# Q6
+# Bob's boss is obsessed with the letter ‘z'
+# Bob's boss wants him to write a program that counts the number of z's
+# in a given string. He provided a test string with five z's...
+
+testString = 'Zebras ZOOM in zig-zag patterns to escape lions with zeal.'
+
+
+# Q7
+# Using your knowledge of the OS module, help Alice make a loading animation.
+# With the clear screen command, you can rapidly reset terminal output.
+# Write three print statements with three separate animations.
+# These animations will flow into each other...
+# Next, write a loop that runs 15 times.
+# Have your loop iterate through the three animations.
+# After each animation is printed, used the clear command.
+# Note: You may need to add a delay between each animation, or add more iterations to the loop.
+
+
+# HW
+# Bob received several more files that need formatting.
+# All of the file names have the string “whisker” in them.
+# Using your knowledge of the OS library…
+# Help Bob write a program that will check the current directory
+# for any files that contain “whisker” in the name
+# and print them to the terminal.
+# Bonus:
+# Instead, for each file that matches, the file name should be added to a list.
+# Lastly, the list should be printed as the output of Bob's program.
+
