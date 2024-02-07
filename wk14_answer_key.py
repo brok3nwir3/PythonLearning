d886f22fc06a2b85f6ef2bfa8e7cfcb2a7b18cdd
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2024-02-06 20:47:27 -0800

    wk14 answer key

---
+++ wk14_answer_key.py
@@ -0,0 +1,31 @@
+# Week 14 - Answer Key
+
+# Q1
+# C) list
+
+# Q2
+# E) A list of lists.
+
+# Q3
+# E) A grid of daily temperature spikes.
+
+# Q4
+# A) True
+
+# Q5
+# E) [0, 0, 0, 0, 0]
+
+# Q6
+# D) 10
+
+# Q7
+# B) [[11, 12, 5, 2], [15, 6, 10], [0, 5, 11, 13, 6], [10, 8, 12, 5], [12, 15, 8, 6]]
+
+# Q8
+# B) Naive Methods
+
+# Q9
+# E) [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]
+
+# Q10
+# B) Matrix[6][0] = 3
