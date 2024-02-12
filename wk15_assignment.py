4f787ba369e32e16ed745ca93c4e467cfb4671d9
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2024-02-12 09:55:47 -0800

    wk15

--- wk15_assignment.py
+++ wk15_assignment.py
@@ -12,8 +12,9 @@
 
 # Q2
 # Alice needs help making a 1D and 2D array using the naive method.
-# Her 1D array needs to be 4 in size, with each entry filled with a '1'.
+# Her 1D array needs to be 4 columns in size, with each entry filled with a '1'.
 # Help Alice make the 1D array.
+# Print the array to make sure it looks alright.
 
 
 
@@ -21,9 +22,35 @@
 # Alice needs help making a 2D array using the naive method.
 # Her 2D array needs to be 4x4 in size, with each entry filled with a '1'.
 # Help Alice make the 2D array.
+# Print the array to make sure it looks alright.
 
 
 
 # Q4
+# Using the code from Q3, attempt to set the entry at [0][0] to 2.
+# Print the updated 2D array and see what the result shows.
+# Can you see the problem that comes with making 2D arrays with the naive method?
 
 
+
+# Q5
+# Bob needs to create a 6x6 2D array, but wants to avoid the naive method.
+# He heard of an approach called "List Comprehension."
+# Bob wants to first initialize a single list, which will be the outter array of the 2D array.
+# He then wants to use a nested for loop to populate the two arrays.
+# You'll need to initialize a second list inside the first loop.
+# Then you'll need to populate each array entry with '1', using the .append() function.
+# Help Bob make his 2D array.
+
+
+# Q6
+# Using the code from Q5, attempt to set the entry at [0][0] to 2.
+# Print the updated 2D array and see what the result shows.
+# Did this style of 2D array creation fix the problem that the naive method had?
+
+
+
+# Q7
+# Read the "Explanation" section of this article:
+# https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
+# See if you can explain the problem with the naive method in your own words.
