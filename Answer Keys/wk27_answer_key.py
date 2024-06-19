bd690fba6e011398840215c33ede8f199b92665d
brok3nwir3 <45925596+brok3nwir3@users.noreply.github.com>
2024-06-18 21:38:08 -0700

    update7

---
+++ Answer Keys/wk27_answer_key.py
@@ -0,0 +1,74 @@
+# Week 27 - Quiz Answer Key
+
+# AVOID PEEKING AHEAD OF TIME!
+
+
+
+
+
+
+
+
+
+
+# Scroll down.
+# |
+# |
+# v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Q1
+# D
+
+# Q2
+# C
+
+# Q3
+# D
+
+# Q4
+# A
+
+# Q5
+# E
+
+# Q6
+# D
+
+# Q7
+# A
+
+# Q8
+# D
+
+# Q9
+# B
+
+# Q10
+# D
